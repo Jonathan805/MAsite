f42ce041c96e792a3fb1ad6be3fb4eb92ec5c3ba
Jonathan805 <risingmidnight@ucla.edu>
2015-04-18 15:47:13 -0700

    fixed button, centered title

--- static/monte.js
+++ static/monte.js
@@ -1,1 +1,14 @@
-function
+ $("#urlButton").click(goToRoom);
+
+ $('#urlText').bind('keypress', function(e) {
+	if(e.keyCode==13){
+		goToRoom();
+	}
+});
+
+function goToRoom()
+{
+	var url = $("#urlText").val() || room_id;
+	url = url.trim().replace(/\s+/g, '-').replace(/[^\w-]/g,'').replace(/-+/g, '-').replace(/^-+/g,'').replace(/-+$/g,'').substr(0, 32) || room_id;
+	document.location.href = "/" + url + "/host";
+}
