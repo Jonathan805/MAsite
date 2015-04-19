2b5cb2500eee54a71b096e324135e9886e86e6c3
Jonathan805 <risingmidnight@ucla.edu>
2015-04-18 22:37:24 -0700

    changed enter key on text

--- static/monte.js
+++ static/monte.js
@@ -1,13 +1,13 @@
  $(document).ready(function(){
  	 $("#urlButton").click(Kappa);
-	$('#normalText').bind('keypress', function(e) {
+	
+	
+ });
+ $('#normalText').bind('keypress', function(e) {
 		if(e.keyCode==13){
 			Kappa();
 		}
 	});
-	
- });
- 
 
 //  $('#normalText').bind('keypress', function(e) {
 // 	if(e.keyCode==13){
